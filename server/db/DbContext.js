c4bf3506266359774de9c73a75b05cd4fd834fdc
daniel le <daniel97.le@gmail.com>
2022-09-23 14:33:24 -0600

    views

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -2,11 +2,13 @@ import mongoose from "mongoose";
 import { AccountSchema } from "../models/Account";
 import { ClipSchema } from "../models/Clip.js";
 import { ValueSchema } from "../models/Value";
+import { ViewerSchema } from "../models/Viewer.js";
 
 class DbContext {
   Values = mongoose.model("Value", ValueSchema);
   Account = mongoose.model("Account", AccountSchema);
   Clips = mongoose.model("Clip", ClipSchema);
+  Viewers = mongoose.model("Viewer", ViewerSchema);
 }
 
 export const dbContext = new DbContext();
--- server/models/Clip.js
+++ server/models/Clip.js
@@ -7,6 +7,15 @@ export const ClipSchema = new Schema(
     name: { type: String, required: true, maxlength: 30 },
     description: { type: String, default: "", maxlength: 200 },
     url: { type: String, required: true },
+    thumbnailURL: {type: String, default: 'https://th.bing.com/th/id/OIP.dZjLz42vJR4eQXuBA4kDigHaEK?pid=ImgDet&rs=1'},
+    views:{type: Number, default: 0},
+    
+
+
+
+
+
+
     // STUB be to sure add whats appropriate
     creatorId: { type: ObjectId, ref: "Account", required: true },
   },
