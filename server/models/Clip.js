b69d8c4ae1443e9f6a82ba812e5262a3a6ac6c68
daniel le <daniel97.le@gmail.com>
2022-09-23 14:53:55 -0600

    commit

--- server/models/Clip.js
+++ server/models/Clip.js
@@ -7,14 +7,13 @@ export const ClipSchema = new Schema(
     name: { type: String, required: true, maxlength: 30 },
     description: { type: String, default: "", maxlength: 200 },
     url: { type: String, required: true },
-    thumbnailURL: {type: String, default: 'https://th.bing.com/th/id/OIP.dZjLz42vJR4eQXuBA4kDigHaEK?pid=ImgDet&rs=1'},
-    views:{type: Number, default: 0},
-    
-
-
-
-
-
+    thumbnailURL: {
+      type: String,
+      default:
+        "https://th.bing.com/th/id/OIP.dZjLz42vJR4eQXuBA4kDigHaEK?pid=ImgDet&rs=1",
+    },
+    views: { type: Number, default: 0 },
+    comments: {},
 
     // STUB be to sure add whats appropriate
     creatorId: { type: ObjectId, ref: "Account", required: true },
@@ -27,14 +26,12 @@ ClipSchema.virtual("creator", {
   foreignField: "_id",
   justOne: true,
   ref: "Account",
-})
+});
 ClipSchema.virtual("viewer", {
   localField: "_id",
   foreignField: "clipId",
   count: true,
-  ref: "Viewer"
-})
-  ;
+  ref: "Viewer",
+});
 
 // TODO add this to dbContext
-
