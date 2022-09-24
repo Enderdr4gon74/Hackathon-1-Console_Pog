1a3b56a412e706487aa607f06819504a18ccef1a
daniel le <daniel97.le@gmail.com>
2022-09-23 19:58:57 -0600

    commit

--- client/app/Models/ActiveClip.js
+++ client/app/Models/ActiveClip.js
@@ -11,7 +11,7 @@ export class ActiveClip {
   }
 
   get activeClipTemplate() {
-    return `
+    return /*html */ `
 
             <div class="modal-content">
           <div class="modal-header">
@@ -48,9 +48,9 @@ export class ActiveClip {
     <div class="col-12 comment">
       <div class="row flex-column justify-content-between comment">
         <div class="col-12 comment-row overflow-auto">
-          <div class="row gap-2">
-            ${this.commentsTemplate}
-           </div>
+          <div class="row gap-2" id="comments-template">
+          //
+          </div>
           </div>
           <div class="col-12 p-0 mt-2">
             <form class="d-flex justify-content-between align-items-center">
@@ -70,51 +70,6 @@ export class ActiveClip {
           </div>
     
     `;
-
-    //   return /*html*/ `
-    //   <div class="col-8 d-flex flex-column justify-content-between">
-    //     <section class="row">
-    //       <div class="col-12">
-    //         <iframe width="100%" height="450" src="${this.url}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    //         <!-- <video width="320" height="240" controls>
-    //           <source src="../client/assets/img/production ID_4247319.mp4" type="video/mp4">
-    //         </video> -->
-    //       </div>
-    //     </section>
-    //     <section class="row mb-2">
-    //       <div class="col-12 d-flex justify-content-between">
-    //         <div class="d-flex">
-    //           <button onclick="" class="btn btn-outline-dark me-4"><i class="mdi mdi-thumb-up"></i>${this.likes}</button>
-    //           <button onclick="" class="btn btn-outline-dark"><i class="mdi mdi-thumb-down"></i>${this.dislikes}</button>
-    //         </div>
-    //         <h4>${this.views}  Views</h4>
-    //       </div>
-    //     </section>
-    //   </div>
-    //   <div class="col-4" id="comments">
-    //   <div class="row d-flex flex-column justify-content-start align-items-stretch comment">
-    //   <div class="col-12 comment">
-    //     <div class="row flex-column justify-content-between comment">
-    //       <div class="col-12 comment-row overflow-auto">
-    //         <div class="row gap-2">
-    //           ${this.commentsTemplate}
-    //          </div>
-    //         </div>
-    //         <div class="col-12 p-0 mt-2">
-    //           <form class="d-flex justify-content-between align-items-center">
-    //             <div class="form-floating">
-    //               <input type="text" class="form-control" name="comment" id="comment" placeholder="Comment">
-    //               <label for="comment">Comment</label>
-    //               </div>
-    //               <button type="submit" class="btn btn-info comment me-2">Comment</button>
-    //           </form>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-    //   </div>
-    //   `;
   }
   get commentsTemplate() {
     return `
--- client/app/Services/ClipsService.js
+++ client/app/Services/ClipsService.js
@@ -1,13 +1,16 @@
 import { appState } from "../AppState.js";
 import { ActiveClip } from "../Models/ActiveClip.js";
 import { Clip } from "../Models/Clip.js";
+import { Comment } from "../Models/Comment.js";
 import { server } from "./AxiosService.js";
 
 class ClipsService {
   async setActiveClip(id) {
     const res = await server.get(`/api/clips/${id}`);
-    console.log(res.data);
+    const response = await server.get(`/api/clips/${id}/comments`);
+    appState.activeComments = new Comment(response.data);
     appState.activeClip = new ActiveClip(res.data);
+    console.log(appState.activeComments);
   }
   async getClips() {
     const res = await server.get("/api/clips");
